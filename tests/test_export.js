47cd25864492ef77e4e099ccf7301666f5326803
Vivek T Ramamoorthy <vivektramamoorthy@gmail.com>
2022-05-06 19:42:03 +0200

    comment added to publish npm package

--- tests/test_export.js
+++ tests/test_export.js
@@ -5,3 +5,7 @@ let MATLABJS =require("../node/main.js")
 
 console.log(MATLABJS.isfield(MATLABJS,"clc")) // true
 console.log(MATLABJS.add(1,22)); // 23
+
+// if this and the test is successful, follow the steps to update the published package
+// npm version 0.x.x
+// npm publish
